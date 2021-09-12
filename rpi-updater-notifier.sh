6051d23472b70d64a548e217a332a38c6f440d99
Yuri Fugi <yuri@yuri.eti.br>
2021-09-12 09:52:49 -0300

    fixes formatting

--- rpi-updater-notifier.sh
+++ rpi-updater-notifier.sh
@@ -2,29 +2,35 @@
 
 MAIL_TO="YOR_EMAIL[at]gmail.com"
 LOG_FILE="/var/log/rpi-update-status.log"
-SUBJECT="[$(/usr/bin/hostname)] Status # apt update $DATA_INICIO" 
+SUBJECT="[$(/usr/bin/hostname)] Status apt update $DATA_INICIO" 
 
 {
     /usr/bin/echo "subject: $SUBJECT"
-    /usr/bin/echo "<pre>"
+    /usr/bin/echo
     /usr/bin/date
+    /usr/bin/echo
     /usr/bin/echo "# apt-get update"
     /usr/bin/apt-get update 2>&1
+    /usr/bin/echo
     /usr/bin/echo "# apt-get upgrade -y"
     /usr/bin/apt-get upgrade -y 2>&1
+    /usr/bin/echo
     /usr/bin/echo "# apt autoremove -y"
     /usr/bin/apt autoremove -y 2>&1 
+    /usr/bin/echo
     /usr/bin/echo "# apt clean -y"
     /usr/bin/apt clean -y 2>&1 
     if [ -f  /var/run/reboot-required ]
     then
+        /usr/bin/echo
         /usr/bin/echo "#################################"
         /usr/bin/echo "[$(hostname)] Needs reboot!"
-        /usr/bin/echo "#################################"        
+        /usr/bin/echo "#################################"
+        /usr/bin/echo
     fi
     /usr/bin/df --local --output=source,pcent --exclude-type=tmpfs
+    /usr/bin/echo
     /usr/bin/date
-    /usr/bin/echo "</pre>"
     /usr/bin/echo "."
 } > "$LOG_FILE"
 
