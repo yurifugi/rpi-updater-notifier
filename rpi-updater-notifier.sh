31c1a0459a82a79cd2e2a490c32414822dd61b80
Yuri Fugi <yuri@yuri.eti.br>
2021-10-16 11:07:58 -0300

    added pihole -up

--- rpi-updater-notifier.sh
+++ rpi-updater-notifier.sh
@@ -28,6 +28,9 @@ SUBJECT="[$(/usr/bin/hostname)] Status apt update $DATA_INICIO"
         /usr/bin/echo "#################################"
         /usr/bin/echo
     fi
+    /usr/bin/echo "# /usr/local/bin/pihole -up"
+    /usr/local/bin/pihole -up 2>&1 
+    /usr/bin/echo 
     /usr/bin/df --local --output=source,pcent --exclude-type=tmpfs
     /usr/bin/echo
     /usr/bin/date
